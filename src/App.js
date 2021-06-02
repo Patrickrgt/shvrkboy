c1f89a7d1f19d583077af7b47abb860d1245608d
Patrickrgt <patrickrgt@gmail.com>
2021-06-02 15:24:47 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+import Parra from "./components/parra.jsx";
+import Mobile from "./components/mobile.jsx";
 
 function App() {
+  function UserGreeting(props) {
+    if (window.innerWidth >= 426) {
+      return <Parra></Parra>;
+    } else if (window.innerWidth <= 425) {
+      return <Mobile></Mobile>;
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserGreeting></UserGreeting>
     </div>
   );
 }
